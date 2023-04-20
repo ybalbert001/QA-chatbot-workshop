9c93e3a4e40f5af812ac8b6c97921a67e4eada09
qingyuan18 <qingyuan18@gmail.com>
2023-04-20 18:01:57 +0800

    add langchain&aos&knn codes

--- code/func.py
+++ code/func.py
@@ -6,6 +6,14 @@ from collections import defaultdict
 from requests_aws4auth import AWS4Auth
 import os
 from opensearchpy import OpenSearch, RequestsHttpConnection
+from langchain.vectorstores import OpenSearchVectorSearch
+from langchain import PromptTemplate, SagemakerEndpoint
+from langchain.chains.question_answering import load_qa_chain
+from langchain.embeddings import SagemakerEndpointEmbeddings
+from langchain.llms.sagemaker_endpoint import ContentHandlerBase
+from langchain.docstore.document import Document
+from langchain.memory import ConversationBufferWindowMemory
+from langchain import LLMChain
 
 source_includes = ["question","answer"]
 runtime= boto3.client('runtime.sagemaker')
@@ -140,7 +148,7 @@ def k_nn_ingestion_by_aos(docs,index,hostname,username,passwd):
         search.index(index=index, body=document)
 
 
-    ########k-nn ingestion by lanchain #########################
+########k-nn ingestion by lanchain #########################
 # input:
 #  docs:ingestion source documents
 #  vectorStore: lanchain AOS vectorStore instance
@@ -150,3 +158,39 @@ def k_nn_ingestion_by_aos(docs,index,hostname,username,passwd):
 def k_nn_ingestion_by_lanchain(docs,vectorStore):
     for doc in docs:
         opensearch_vector_search.add_texts(docs,batch_size=10)
+
+class ExtractContentHandler(ContentHandlerBase):
+    content_type = "application/json"
+    accepts = "application/json"
+
+    def transform_input(self, prompt: str, model_kwargs: Dict) -> bytes:
+        input_str = json.dumps({"ask": prompt})
+        return input_str.encode('utf-8')
+
+    def transform_output(self, output: bytes) -> str:
+        model_predictions = json.loads(output.read().decode("utf-8"))
+        generated_text = model_predictions["answer"]
+        return generated_text
+########topic extraction by lanchain sm endpoint #########################
+# input:
+#  docs: history QA list[(Q1,A1).(Q2,A2),(Q3,A3)]
+#  k: output extracted words limit
+#  llm: lanchain llm instance
+# return:
+#  result : output extracted features
+#############################################################
+def feature_extraction_by_lanchain(docs,k,sm_endpoint_nm,sm_region):
+    feature_extraction_handler = ExtractContentHandler()
+    feature_extraction_llm=SagemakerEndpoint(
+        endpoint_name=sm_endpoint_nm,
+        region_name=sm_region,
+        content_handler=feature_extraction_handler
+    )
+    payload = ""
+    for doc in docs:
+        question, answer = doc
+        payload=payload+"Q:"+question+"\n"
+        payload=payload+"A:"+answer+"\n"
+    payload = payload+"问题：主题摘要,限制在"+str(k)+"个字内"
+    extracted_feature_texts=feature_extraction_llm(payload1)
+    return extracted_feature_texts
